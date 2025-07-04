6dabce6d34e55c2af2e4461314f02155ffad6992
AkaraChen <akarachen@outlook.com>
2025-07-04 16:34:01 +0800

    feat: lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,80 +1,11 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
 lvim.plugins = {
-    {
-  "phaazon/hop.nvim",
-  event = "BufRead",
-  config = function()
-    require("hop").setup()
-    vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-    vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-  end,
-},
-{
-  "ggandor/leap.nvim",
-  name = "leap",
-  config = function()
-    require("leap").add_default_mappings()
-  end,
-},
-{
-  'wfxr/minimap.vim',
-  build = "cargo install --locked code-minimap",
-  -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-  config = function ()
-    vim.cmd ("let g:minimap_width = 10")
-    vim.cmd ("let g:minimap_auto_start = 1")
-    vim.cmd ("let g:minimap_auto_start_win_enter = 1")
-  end,
-},{
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      window = {
-        width = 30,
-      },
-      buffers = {
-        follow_current_file = true,
-      },
-      filesystem = {
-        follow_current_file = true,
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_by_name = {
-            "node_modules"
-          },
-          never_show = {
-            ".DS_Store",
-            "thumbs.db"
-          },
-        },
-      },
-    })
-  end
-},
-{
-  "karb94/neoscroll.nvim",
-  event = "WinScrolled",
-  config = function()
-  require('neoscroll').setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
-        mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-        '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-        hide_cursor = true,          -- Hide cursor while scrolling
-        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil,        -- Default easing function
-        pre_hook = nil,              -- Function to run before the scrolling animation starts
-        post_hook = nil,              -- Function to run after the scrolling animation ends
-        })
-  end
-},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
+
+lvim.colorscheme = "catppuccin-frappe"
